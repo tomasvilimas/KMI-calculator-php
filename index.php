e4a79f15533fd76bc98c8fa3d2bb27cc42d8a09f
tomasvilimas <74533072+tomasvilimas@users.noreply.github.com>
2021-02-23 19:37:40 +0200

    update2

--- index.php
+++ index.php
@@ -5,16 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
 <?php
 print("<pre>");
 
 $ugis= $_POST["ugis"];
 $svoris= $_POST["svoris"];
 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(!isset($_POST['ugis']) || empty($_POST['ugis']))
+      print('<h3 style="color: red">Įveskite savo ūgį</h3>');
+    
+      
+    if(!isset($_POST['svoris']) || empty($_POST['svoris']))
+      print('<h3 style="color: red">Įveskite savo svorį</h3>');
+    
+
+
 If($svoris/(($ugis*$ugis)/10000)<18.5)
-    print('nepakankamas svoris');
+    print('nepakankamas svoris' );
     else if($svoris/(($ugis*$ugis)/10000)<25)
     print('normalus svoris');
     else if($svoris/(($ugis*$ugis)/10000)<30)
@@ -22,15 +35,17 @@ If($svoris/(($ugis*$ugis)/10000)<18.5)
     else if($svoris/(($ugis*$ugis)/10000)>30)
     print('Nutukimas');
 
+}
+
 print("</pre>");
 
 ?>
 
-<form action="./" method="POST">  
-<label for="fname">Jūsų ūgis (cm):</label><br>
-<input type="text" id="fname" name="ugis" value=""><br>
+<form  class="label" action="./" method="POST">  
+<label  for="fname">Jūsų ūgis (cm):</label><br>
+<input  type="text" id="<?php echo 'ugis'; ?>" name="ugis" value="<?php if(isset($_POST['ugis'])) print($_POST['ugis']) ?>"><br>
 <label for="lname">Jūsų svoris (kg):</label><br>
-<input type="text" id="lname" name="svoris" value=""><br>
+<input type="text" id="<?php echo 'svoris'; ?>" name="svoris" value="<?php if(isset($_POST['svoris'])) print($_POST['svoris']) ?>"><br>
 <input type="submit" value="Skaičiuoti">
 </form>
 
